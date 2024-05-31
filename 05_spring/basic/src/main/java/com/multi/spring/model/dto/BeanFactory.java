e1373bae8a2120fee1bc7fe5523d17e8cf86a52b
jmp0422 <rhkdlf6587@naver.com>
2024-05-31 18:11:48 +0900

    2024.05.31 practice

--- 05_spring/basic/src/main/java/com/multi/spring/model/dto/BeanFactory.java
+++ 05_spring/basic/src/main/java/com/multi/spring/model/dto/BeanFactory.java
@@ -3,6 +3,7 @@ package com.multi.spring.model.dto;
 public class BeanFactory {/* 개발자가 원하는 객체를 선택해서 생성하기위한 전용 객체 */
 	
 	public static Object getBean(String name) {
+		int x = 1;
 		Singer singer= null;
 		
 		if(name.equals("twice")){
