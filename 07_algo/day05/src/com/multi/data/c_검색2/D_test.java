86c7f3c2930035bd9ed27054e42341222b5a9300
jmp0422 <rhkdlf6587@naver.com>
2024-06-30 22:24:19 +0900

    test

--- 07_algo/day05/src/com/multi/data/c_검색2/D_test.java
+++ 07_algo/day05/src/com/multi/data/c_검색2/D_test.java
@@ -2,8 +2,9 @@ package com.multi.data.c_검색2;
 
 public class D_test {
     public static void main(String[] args) {
-        int x = 0;
-        x++;
-        System.out.println(x);
+        int y = 0;
+        y++;
+
+        System.out.println(y);
     }
 }
